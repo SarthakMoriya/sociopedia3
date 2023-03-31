98d32df7622e87d2e1ada618d760e055f8111911
Sarthak30-03-2003 <sarthak8544@gmail.com>
2023-04-01 00:37:55 +0530

    login

--- client/src/scenes/loginPage/LoginPage.jsx
+++ client/src/scenes/loginPage/LoginPage.jsx
@@ -27,7 +27,7 @@ const LoginPage = () => {
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
           Welcome to Socipedia, the Social Media for Sociopaths!
         </Typography>
-        {/* <Form /> */}
+        <Form />
       </Box>
     </Box>
   );
